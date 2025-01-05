3b337f5e95c9d94034e7a2174ad9458a622ac0c1
haotianw177e <haotianw177@gmail.com>
2025-01-05 04:49:22 -0500

    1

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,14 +9,31 @@ const Footer = () => {
             <div className="footer-content-left">
                 <img src={assets.logo} alt="" />
                 <p>dummy text</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
             </div>
             <div className="footer-content-center">
-
+                <h2>Company</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
             </div>
             <div className="footer-content-right">
-
+                <h2>Get in touch</h2>
+                <ul>
+                    <li>+1 582 418 9922</li>
+                    <li>contact@tomato.com</li>
+                </ul>
             </div>
         </div>
+        <hr />
+        <p className='footer-copyright'>Copyright 2025 @ tomato.com - All right reserved</p>
     </div>
   )
 }
