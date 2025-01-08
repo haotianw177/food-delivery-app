c690485456a1bde5fce6f4a3b2a9f4b8a8f5aeca
haotianw177e <haotianw177@gmail.com>
2025-01-08 05:16:26 -0500

    1

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
   // The state category (initial value "ALL") and its setter setCategory 
@@ -15,6 +16,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
